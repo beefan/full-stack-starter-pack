cf3b14213cd9a266865ad740debbb018d5b07bd1
Brandon Fannin <bfannin13@gmail.com>
2021-04-11 22:45:59 -0400

    Add routes and test api w Postman

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :widgets, only: [:index, :show, :create, :update, :destroy]
+    resources :sales, only: [:index, :show, :create, :destroy]
+  end
+
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#logout'
+  post '/signup', to: 'sign_ups#create'
 end
