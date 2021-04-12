b4a06fbe12856edbdf8570c3eae636f4d71596dd
Brandon Fannin <bfannin13@gmail.com>
2021-04-12 10:02:58 -0400

    vue add vuex

--- client/src/main.js
+++ client/src/main.js
@@ -6,6 +6,7 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import router from "./router";
+import store from "./store";
 
 // Use Bootstrap and optional icons plugin in the project
 Vue.use(BootstrapVue);
@@ -15,5 +16,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
